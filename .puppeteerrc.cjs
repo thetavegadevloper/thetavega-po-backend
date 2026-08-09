c618f5832b4fccb4dedeefc3597a1c88d68c8eba
Prajakta Vyawahare <prajaktavyawahare118@gmail.com>
2026-08-09 13:07:51 +0530

    Fix Puppeteer Chrome cache for Render

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,10 @@
+const path = require("path");
+
+/** @type {import("puppeteer").Configuration} */
+module.exports = {
+  cacheDirectory: path.join(
+    __dirname,
+    ".cache",
+    "puppeteer"
+  )
+};
